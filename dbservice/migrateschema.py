84cd17828ef89e1916db2f04f7d492c44f430b5f
chunze605 <62882048+chunze605@users.noreply.github.com>
2021-06-18 19:14:08 +0800

    Create migrateschema.py

---
+++ dbservice/migrateschema.py
@@ -0,0 +1,36 @@
+from dbservice.oracle.oracleconnector import OracleConnector
+from dbservice.mariadb.mariadbconnector import MariadbConnector
+from dbservice.oracle.schemamapping import create_table_statement_migrate
+
+
+# Extraction on table description
+list_tables_query = """SELECT DBMS_METADATA.GET_DDL('TABLE',u.table_name)
+     FROM USER_ALL_TABLES u
+     WHERE u.nested='NO'
+     AND (u.iot_type is null or u.iot_type='IOT')"""
+
+create_table_statement_list = []
+
+# Instantiate an Oracle connection
+oracle_connector = OracleConnector()
+
+# Schema Transformation
+for row in oracle_connector.execute(list_tables_query):
+    create_table_statement_list.append(create_table_statement_migrate(row[0]))
+
+# Instantiate an MariaDB connection
+mariadb_connector = MariadbConnector()
+
+for create_table_statements in create_table_statement_list:
+    for statement in create_table_statements:
+        if statement is not None:
+            mariadb_connector.execute(statement)
+
+
+oracle_connector.close()
+mariadb_connector.close()
+
+
+
+
+
