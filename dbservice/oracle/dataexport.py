652a8e845499d4e601d59eaabc734dc47283fb2b
chunze605 <62882048+chunze605@users.noreply.github.com>
2021-06-28 03:53:34 +0800

    Update dataexport.py

--- dbservice/oracle/dataexport.py
+++ dbservice/oracle/dataexport.py
@@ -1,19 +1,11 @@
-import os
-import csv
 
+import csv
 def get_data(connector, schema, table) -> object:
     statement = f"""select * from {schema}.{table}"""
-
     data = connector.execute(statement)
     return data
 
 def write_to_file(data, path):
-    # f = open(path, "w+")
-    # for row in data:
-    #     f.write(str(row))
-    # f.close()
-    data = [list(i) for i in data]
-    print(data)
 
     with open(path,"w",newline="") as f:
         write = csv.writer(f)
