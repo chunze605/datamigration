fee4d4ba7d9a3ea5fddeea4c9ddac8f4b1f09e47
chunze605 <62882048+chunze605@users.noreply.github.com>
2021-06-28 03:49:15 +0800

    Update migratedata.py
    convert to function migrate_data for main.py

--- dbservice/migratedata.py
+++ dbservice/migratedata.py
@@ -7,47 +7,37 @@ import os
 
 
 
-#staging_path = f'staging_data\table\{datetime.now().date().__str__()}.csv' # a file path where you want to store ur csv file
+def migrate_data():
+    staging_path = rf'{os.path.abspath(os.getcwd())}\dbservice\staging_data\{datetime.now().date().__str__()}.csv' # a file path where you want to store ur csv file
+    list_tables_query = """select table_name from tabs"""
+    oracle_connector = OracleConnector()
+    mariadb_connector = MariadbConnector()
 
-staging_path = rf'C:\Users\Chun\Desktop\MyFiles\sem9\fyp\app\dbservice\staging_data\table\{datetime.now().date().__str__()}.csv' # a file path where you want to store ur csv file
-
-#list tables name
-# list_tables_query = """SELECT u.schema, u.table_name
-#      FROM USER_ALL_TABLES u
-#      WHERE u.nested='NO'
-#      AND (u.iot_type is null or u.iot_type='IOT')"""
-
-list_tables_query = """select table_name from tabs"""
-
-
-oracle_connector = OracleConnector()
-mariadb_connector = MariadbConnector()
-
-for row in oracle_connector.execute(list_tables_query):
-    # get data
-    # schema = row[0]
-    # table = row[1]
-    table = str(row[0])
-    data = get_data(oracle_connector,"admin",table) # only 1 schema is used, multiple schema migration still in process
+    for row in oracle_connector.execute(list_tables_query):
+        # get data
+        # schema = row[0]
+        # table = row[1]
+        table = str(row[0])
+        data = get_data(oracle_connector,"admin",table) # only 1 schema is used, multiple schema migration still in progress
 
 
-    # write to staging file
-    write_to_file(data,staging_path)
+        # write to staging file
+        write_to_file(data,staging_path)
 
-    # load data
-    mariadb_connector.execute("""set foreign_key_checks=0""")
-    load_data(mariadb_connector, table, staging_path)
-    mariadb_connector.execute("""set foreign_key_checks=1""")
+        # load data
+        mariadb_connector.execute("""set foreign_key_checks=0""")
+        load_data(mariadb_connector, table, staging_path)
+        mariadb_connector.execute("""set foreign_key_checks=1""")
 
-    # clear/delete folder
-    os.remove(staging_path)
-    # break
+        # clear/delete folder
+        os.remove(staging_path)
+        # break
+
+        # rmtree(staging_path)
 
 
-    # rmtree(staging_path)
-
-mariadb_connector.close()
-oracle_connector.close()
+    mariadb_connector.close()
+    oracle_connector.close()
 
 
 
